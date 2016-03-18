386718efa0acd3f2ea97cfb3f6524fdf8ccd7366
ethesx <ethesx@gmail.com>
2016-03-18 15:09:22 -0400

    Added flag

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,7 @@
 Constants = {
     origin : [
-        {url : "aHR0cDovL3NlYXJjaC5iYXJuZXNhbmRub2JsZS5jb20vYm9va3NlYXJjaC9pc2JuSW5xdWlyeS5hc3A/ej15JkVBTj0+", name : BNTermEnum.BN, inactive : false},
-        {url : "aHR0cHM6Ly93d3cuY29tbW9uc2Vuc2VtZWRpYS5vcmcvYm9vay1yZXZpZXdzLw==", name : "", inactive : true}
+        {url : "aHR0cDovL3NlYXJjaC5iYXJuZXNhbmRub2JsZS5jb20vYm9va3NlYXJjaC9pc2JuSW5xdWlyeS5hc3A/ej15JkVBTj0+", name : BNTermEnum.BN, isbnSupport : true, inactive : false},
+        {url : "aHR0cHM6Ly93d3cuY29tbW9uc2Vuc2VtZWRpYS5vcmcvYm9vay1yZXZpZXdzLw==", name : "", isbnSupport : false, inactive : true}
     ],
 
 };
